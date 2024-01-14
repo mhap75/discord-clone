b68058e275b73157e03309070b44c52213ef287f
Mohammad Hossein Amirpanahi <mhap75@yahoo.com>
2024-01-14 06:58:44 +0330

    Chat

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/ChatHeader";
+import ChatInput from "@/components/chat/ChatInput";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -34,6 +35,16 @@ const ChannelById = async ({
         serverId={channel.serverId}
         name={channel.name}
         type="channel"
+      />
+      <div className="flex-1">Future messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
